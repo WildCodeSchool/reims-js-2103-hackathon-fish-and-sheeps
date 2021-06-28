e7ad950577d83e665409a9618b8392483bc567eb
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-06-28 17:14:25 +0200

    Installed fs - cors - multer + added upload fonctionality + sample upload code in front-end

--- Back/app.js
+++ Back/app.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
-
 const express = require('express');
 
 const api = require('./api');
 const app = express();
+const cors = require('cors');
 
 
 app.use(express.json());
+app.use(cors());
 app.use('/api', api);
 
 app.get('/', (req, res) => {
