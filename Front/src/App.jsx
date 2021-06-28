c8636c30cbda5933ce4d167ed6cc29ea2b6d5cf8
BrianBTI <bertili.brian@gmail.com>
2021-06-28 14:32:40 +0200

    Setup esLint and Prettier

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './pages/Home'
+import Home from './pages/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-          <Switch>
-            <Route path="/" exact component={Home} />
-          </Switch>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
