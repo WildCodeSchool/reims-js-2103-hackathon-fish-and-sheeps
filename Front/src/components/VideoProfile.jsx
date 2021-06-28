151b826919b06d00263f8aee81e63a5b3821029e
BrianBTI <bertili.brian@gmail.com>
2021-06-28 15:59:48 +0200

    map on array to display user's video on his profile

---
+++ Front/src/components/VideoProfile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function VideoProfile() {
+  return (
+    <div className="user__video">
+      <p>Video goes here</p>
+    </div>
+  );
+}
+
+export default VideoProfile;
--- Front/src/pages/Profile.jsx
+++ Front/src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import NavBar from "../components/NavBar.jsx";
 import FollowBar from "../components/FollowBar.jsx";
+import VideoProfile from "../components/VideoProfile.jsx";
 
 import "./Profile.css";
 
@@ -12,7 +13,24 @@ const ProfileHeader = styled.div`
   padding-top: 4rem;
 `;
 
+const UserContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding-top: 4rem;
+`;
+
 function Profile() {
+  const userVideos = [
+    {
+      id: 1,
+      url: "here",
+    },
+    {
+      id: 2,
+      url: "here",
+    },
+  ];
   return (
     <>
       <NavBar />
@@ -22,6 +40,14 @@ function Profile() {
           <ProfileHeader>
             <img id="pdp" src="" alt="profile picture" />
           </ProfileHeader>
+          <UserContent>
+            {userVideos.map((videoToDisplay) => (
+              <VideoProfile
+                key={videoToDisplay.id}
+                title={videoToDisplay.url}
+              />
+            ))}
+          </UserContent>
         </div>
       </div>
     </>
