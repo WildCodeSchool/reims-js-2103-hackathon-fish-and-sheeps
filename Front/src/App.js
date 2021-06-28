21f777ed60d713cd0303b7dad1f8e685646a3071
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-06-28 17:16:38 +0200

    missing file App.js in previous commit

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState();
+
+  const changeHandler = (event) => {
+		setSelectedFile(event.target.files[0]);
+	};
+
+	const handleSubmission = () => {
+    const formData = new FormData();
+		formData.append('File', selectedFile);
+
+		fetch(
+			'http://localhost:8000/api/upload',
+			{
+				method: 'POST',
+				body: formData,
+			}
+		)
+			.then((response) => response.json())
+			.then((result) => {
+				console.log('Success:', result);
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+			<input type="file" name="file" onChange={changeHandler} />
+      {selectedFile != null ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+          <p>Size in bytes: {selectedFile.size}</p>
+          <p>
+            lastModifiedDate:{' '}
+            {new Date(selectedFile.lastModified).toLocaleDateString()}
+          </p>
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
+			<div>
+				<button onClick={handleSubmission}>Submit</button>
+			</div>
+		</div>
   );
 }
 
