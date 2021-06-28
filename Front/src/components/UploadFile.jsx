bf9bda90d5c131123a0568528faa347ac9a7aefe
BrianBTI <bertili.brian@gmail.com>
2021-06-28 17:35:49 +0200

    Created component UploadFile

---
+++ Front/src/components/UploadFile.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "../App.css";
+
+function App() {
+  const [selectedFile, setSelectedFile] = useState();
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleSubmission = () => {
+    const formData = new FormData();
+    formData.append("File", selectedFile);
+
+    fetch("http://localhost:8000/api/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div>
+      <input type="file" name="file" onChange={changeHandler} />
+      {selectedFile != null ? (
+        <div>
+          <p>Filename: {selectedFile.name}</p>
+          <p>Filetype: {selectedFile.type}</p>
+          <p>Size in bytes: {selectedFile.size}</p>
+          <p>
+            lastModifiedDate:{" "}
+            {new Date(selectedFile.lastModified).toLocaleDateString()}
+          </p>
+        </div>
+      ) : (
+        <p>Select a file to show details</p>
+      )}
+      <div>
+        <button onClick={handleSubmission}>Submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
